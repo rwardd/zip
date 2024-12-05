98d72ff70d8fc44ebadc7296bf559fe4332e11a9
Ryan Ward <ryan.c.ward3@boeing.com>
2024-12-05 14:46:46 +1000

    cleaned up stack allocation a little

--- src/init.zig
+++ src/init.zig
@@ -3,10 +3,11 @@ const sched = @import("sched/sched.zig");
 const logger = @import("log.zig");
 
 fn hello1(args: ?*anyopaque) void {
-    const cnt: u32 = 1;
+    var cnt: u32 = 1;
     while (true) {
         logger.log("Hello from task 1 ");
         log_number(cnt);
+        cnt = (cnt + 1) % 0x70; // loop back round without overflowing u8
         sched.save_sp();
         sched.yield();
     }
@@ -39,23 +40,19 @@ fn log_number(x: u32) void {
     logger.log("\n");
 }
 
+// Maybe look at some kind of dynamic allocation
+var thread1_stack = task.create_stack(256); //[_]u8{0} ** 256;
+var thread2_stack = task.create_stack(256); //[_]u8{0} ** 256;
+var thread3_stack = task.create_stack(256); //[_]u8{0} ** 256;
+
 export fn start() noreturn {
-    // BIG TODO: Look at a better way to store task structures
-    // Not a fan of just keeping them here on the start stack
-    // Should be defined in memory region ???
+    var thread1 = task.create(&hello1, 3, &thread1_stack);
+    var thread2 = task.create(&hello2, 2, &thread2_stack);
+    var thread3 = task.create(&hello3, 1, &thread3_stack);
 
-    // Move this to specific linker section to avoid overflowing initial stack
-    var thread1_stack = [_]u8{0} ** 256;
-    var thread2_stack = [_]u8{0} ** 256;
-    var thread3_stack = [_]u8{0} ** 256;
-
-    var thread1 = task.task_create(&hello1, 3, &thread1_stack);
-    var thread2 = task.task_create(&hello2, 2, &thread2_stack);
-    var thread3 = task.task_create(&hello3, 1, &thread3_stack);
-
-    task.task_init(&thread1);
-    task.task_init(&thread2);
-    task.task_init(&thread3);
+    task.init(&thread1);
+    task.init(&thread2);
+    task.init(&thread3);
 
     log_number(task.get_thread_tcb(0).?.priority);
     log_number(task.get_thread_tcb(1).?.priority);
