ce5ea7ab965bc9ce7ec7d81264bfe5e7e969e8b8
rwardd <ryan.ward17@outlook.com>
2025-01-07 23:27:19 +1000

    Updated build.zig and added arm linker script

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ const Feature = std.Target.Cpu.Feature;
 pub fn build(b: *std.Build) void {
     const debug = b.option(bool, "debug", "Run qemu in debug mode") orelse false;
 
-    const arch_opt = b.option([]const u8, "architecture", "The target architecture") orelse "rv32";
+    const arch_opt = b.option([]const u8, "architecture", "The target architecture") orelse "arm_cortex_m0";
     const arch = std.meta.stringToEnum(Arch, arch_opt).?;
     const platform_opt = b.option([]const u8, "platform", "The target platform") orelse "qemu_virt";
     const platform = std.meta.stringToEnum(Platform, platform_opt).?;
@@ -39,14 +39,18 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&qemu.step);
 }
 
-const Arch = enum(u32) { rv32, arm_cortex_m0 };
+const Arch = enum(u32) { rv32, arm_cortex_m0, unknown };
 const Platform = enum(u32) { qemu_virt };
 
 const architecture = struct {
     id: Arch,
     path: []const u8,
 };
-const arch_lookup = [_]architecture{architecture{ .id = Arch.rv32, .path = "arch/riscv/rv32/cpu.zig" }};
+
+const arch_lookup = [_]architecture{
+    architecture{ .id = Arch.rv32, .path = "arch/riscv/rv32/cpu.zig" },
+    architecture{ .id = Arch.arm_cortex_m0, .path = "arch/arm/cpu.zig" },
+};
 
 fn get_arch_path(arch: Arch) []const u8 {
     return arch_lookup[@intFromEnum(arch)].path;
@@ -60,6 +64,14 @@ fn select_target(arch: Arch) std.Target.Query {
                 .os_tag = Target.Os.Tag.freestanding,
                 .abi = Target.Abi.none,
                 .cpu_model = .{ .explicit = &std.Target.riscv.cpu.baseline_rv32 },
+            };
+        },
+        Arch.arm_cortex_m0 => {
+            return std.Target.Query{
+                .cpu_arch = Target.Cpu.Arch.arm,
+                .os_tag = Target.Os.Tag.freestanding,
+                .abi = Target.Abi.none,
+                .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_m0 },
             };
         },
         else => {
@@ -76,6 +88,9 @@ fn initialise_architecture(arch: Arch, b: *std.Build, exe: *std.Build.Step.Compi
             exe.addAssemblyFile(b.path("arch/riscv/rv32/qemu_virt/vector.S"));
             exe.setLinkerScriptPath(b.path("arch/riscv/rv32/link.ld"));
         },
+        Arch.arm_cortex_m0 => {
+            exe.setLinkerScriptPath(b.path("arch/arm/link.ld"));
+        },
         else => {},
     }
 }
@@ -91,6 +106,20 @@ fn qemu_config(arch: Arch, board: Platform) []const []const u8 {
                         "virt",
                         "-bios",
                         "none",
+                        "-kernel",
+                        "zig-out/bin/zip",
+                        "-nographic",
+                    };
+                },
+            }
+        },
+        Arch.arm_cortex_m0 => {
+            switch (board) {
+                Platform.qemu_virt => {
+                    return &[_][]const u8{
+                        "qemu-system-arm",
+                        "-machine",
+                        "lm3s6965evb",
                         "-kernel",
                         "zig-out/bin/zip",
                         "-nographic",
